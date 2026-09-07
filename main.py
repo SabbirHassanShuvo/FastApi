59f0cfe245d1e24071502fd8e75507d339cbc45a
SabbirHassanShuvo <sabbirhassanshuvo71@gmail.com>
2026-09-07 22:28:27 +0600

    Learn Python Classes as Models

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Body
 
 app = FastAPI()
 
@@ -19,3 +19,14 @@ def heello_world():
 @app.get("/todos")
 def get_todos():
     return Toods
+
+@app.post("/todos")
+def create_todo(new_todo=Body()):
+    Toods.append(new_todo)
+
+    return {"message": "Todo created successfully", "todo": new_todo}
+
+@app.put("/todos_update")
+def update_todo(exiting_todo=Body()):
+    Toods[0] = exiting_todo
+    return {"message": "Todo updated successfully", "todo": exiting_todo}
